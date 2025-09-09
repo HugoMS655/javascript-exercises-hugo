9875d1042831f242950f2610edd66d09acbda3fb
Hugo <104373242+HugoMS655@users.noreply.github.com>
2025-09-09 14:17:12 +0100

    Add solution to exerccise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function calculateAge(dateOfBirth, dateOfDeath) {
+  if (dateOfDeath === undefined) {
+    dateOfDeath = new Date().getFullYear();
+  }
+  return dateOfDeath - dateOfBirth;
+}
 
+const findTheOldest = function (peopleArray) {
+  let result = 0;
+  let oldest = peopleArray[0];
+  let age;
+  for (let i = 0; i < peopleArray.length; i++) {
+    age = calculateAge(peopleArray[i].yearOfBirth, peopleArray[i].yearOfDeath);
+    if (age > result) {
+      result = age;
+      oldest = peopleArray[i];
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
