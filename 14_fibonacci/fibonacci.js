051ff0dff647dedb369769ff4feb7ca460561c4e
Hugo <104373242+HugoMS655@users.noreply.github.com>
2025-09-09 13:46:23 +0100

    Add solution to ex 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (arg) {
+  let n;
+  if (typeof arg !== "number") {
+    n = Number(arg);
+  } else {
+    n = arg;
+  }
 
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0) {
+    return 0;
+  }
+  const fib = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[n];
 };
 
 // Do not edit below this line
