2ac3c338b92a664ab1ebe1be1de9f0130aa69d3e
Hugo <104373242+HugoMS655@users.noreply.github.com>
2025-09-09 11:53:12 +0100

    Add solution to exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  // ^ means not , g means global, or in other words, replace everything not just first one
+  const cleanString = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
+  const reversedString = cleanString.split("").reverse().join("");
+
+  return cleanString === reversedString;
 };
 
 // Do not edit below this line
